04cd104b6b6dad2c80018ae12b7d8a9d74cac2f4
zizuaine <Itsuki7444@gmail.com>
2026-03-29 19:22:25 +0530

    made it more responsive

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -12,14 +12,12 @@ const Header = () => {
     return (
         <header className="hero-header">
             <nav className={isLightPage ? "floating-nav auth-nav" : "floating-nav"} >
+                <Link className="nav-mark" to="/" aria-label="Lifto home">
+                    <img src="/favicon.svg" width="20" height="20" alt="lifto" />
+                </Link>
                 <Link className="nav-link" to="/">Home</Link>
                 {userToken ? <Link className="nav-link" to="/dashboard">dashboard</Link> : null}
                 <Link className="nav-link" to="/charities">charities</Link>
-
-                <span className="nav-badge">
-                    <img src="/favicon.svg" width="20" height="20" alt="lifto" />
-                </span>
-
                 <Link className="nav-link" to="/admin">admin</Link>
                 {!userToken ? <Link className="nav-link" to="/login">login</Link> : null}
                 <Link className="nav-link" to="/subscribe">subscribe</Link>
